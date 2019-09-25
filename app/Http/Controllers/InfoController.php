288f36edf36edb768d08ddbdfcd1c0a3226a8702
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2019-09-25 11:21:43 +0530

    Add auto-increment-id Year-dayOfYear-24Hour-minutes-seconds-microseconds

---
+++ app/Http/Controllers/InfoController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Google\Cloud\Firestore\FirestoreClient;
+
+class InfoController extends Controller
+{
+    private $_konnectFirestoreClient = [];
+
+    public function __construct()
+    {
+        $this->_konnectFirestoreClient = new FirestoreClient(
+            [
+                'projectId' => env('FIRESTORE_PROJECT_ID', ''),
+                'apiKey' => env('FIRESTORE_WEB_API_KEY', '')
+            ]
+        );
+    }
+
+    public function i()
+    {
+        phpinfo();
+        die();
+    }
+
+    public function fs()
+    {
+        $data_count = rand(10, 20);
+        for ($i = 1; $i <= $data_count; $i++) {
+            $konnection = $this->_konnectFirestoreClient->collection('user_logins')->document(sha1(rand() . time()));
+            $konnection->set([
+                'user_id' => date('YzHisu'),
+                'ip_address' => @$_SERVER['REMOTE_ADDR'],
+                'added' => date('F d, Y H:i:s'),
+                'timezone_added' => date('F d, Y H:i:s') . ' - ' . date('e'),
+            ]);
+        }
+        die('!');
+    }
+
+    public function fr()
+    {
+        $konnection = $this->_konnectFirestoreClient->collection('user_logins');
+        $docs = $konnection->orderBy('added', 'desc')->limit(10)->documents();
+        $_logins = [];
+        foreach ($docs as $doc) {
+            if ($doc->exists()) {
+                $_logins[] = $doc->data();
+            }
+        }
+        return $_logins;
+    }
+}
