2609317ffb92f10b2ad11b91b92b496dcdad8f55
jsucreate <jatindersingh@ucreate.co.in>
2019-12-26 16:15:39 +0530

    testing

--- push.php
+++ push.php
@@ -1,44 +1,48 @@
 <?php
-header('Content-Type: text/event-stream');
-header('Cache-Control: no-cache'); // recommended to prevent caching of event data.
+include './vendor/autoload.php';
+use Sse\Event;
+use Sse\SSE;
 
-// function sendMsg($id, $msg) {
-//   echo "id: $id" . PHP_EOL;
-//   echo "data: $msg" . PHP_EOL;
-//   echo PHP_EOL;
-//   ob_flush();
-//   flush();
-// }
-// $serverTime = time();
-// sendMsg($serverTime, 'server time: ' . date("h:i:s", time()));
+//create the event handler
+class YourEventHandler implements Event {
+    public function update(){
+        //Here's the place to send data
+        return 'Hello, world!';
+    }
+    
+    public function check(){
+        //Here's the place to check when the data needs update
+        return true;
+    }
+}
+
+$sse = new SSE(); //create a libSSE instance
+$sse->addEventListener('event_name', new YourEventHandler());//register your event handler
+$sse->start();//start the event loop
+
+// include './vendor/autoload.php';
+
+// use Hhxsv5\SSE\SSE;
+// use Hhxsv5\SSE\Update;
+
+// //example: push messages to client
 
 // header('Content-Type: text/event-stream');
 // header('Cache-Control: no-cache');
 // header('Connection: keep-alive');
 // header('X-Accel-Buffering: no');//Nginx: unbuffered responses suitable for Comet and HTTP streaming applications
 
-include './vendor/autoload.php';
-
-use Hhxsv5\SSE\SSE;
-use Hhxsv5\SSE\Update;
-
-//example: push messages to client
-
-
-(new SSE())->start(new Update(function () {
-    $id = 1;
-    $newMsgs = [
-        [
-            'id'      => $id,
-            'title'   => 'title' . $id,
-            'content' => 'content' . $id,
-        ],
-    ];//get data from database or service.
-    if (!empty($newMsgs)) {
-        return json_encode(['newMsgs' => $newMsgs]);
-    }
-    return false;//return false if no new messages 
-    ob_flush();
-    flush();   
-}), 'new-msgs');
-
+// (new SSE())->start(new Update(function () {
+//     $id = mt_rand(1, 1000);
+//     $newMsgs = [
+//         [
+//             'id'      => $id,
+//             'title'   => 'title' . $id,
+//             'content' => 'content' . $id,
+//         ],
+//     ];//get data from database or service.
+//     if (!empty($newMsgs)) {
+//         return json_encode(['newMsgs' => $newMsgs]);
+//     }
+//     return false;//return false if no new messages
+// }), 'new-msgs');
