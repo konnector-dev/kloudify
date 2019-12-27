119dfef76a6771cd8147709769aa704785107ca3
jsucreate <jatindersingh@ucreate.co.in>
2019-12-27 10:40:49 +0530

    testing

--- push.php
+++ push.php
@@ -11,9 +11,8 @@ header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 header('Connection: keep-alive');
 header('X-Accel-Buffering: no');//Nginx: unbuffered responses suitable for Comet and HTTP streaming applications
-// echo 'data: '. '--------'. mt_rand(1, 1000). "\n\n";
-// flush();
-(new SSE())->start(new Update(function () {
+
+$data = (new SSE())->start(new Update(function () {
     $id = mt_rand(1, 1000);
     $newMsgs = [
         [
@@ -23,7 +22,9 @@ header('X-Accel-Buffering: no');//Nginx: unbuffered responses suitable for Comet
         ],
     ];//get data from database or service.
     if (!empty($newMsgs)) {
-        return json_encode(['newMsgs' => $newMsgs]). "\n\n";
+        return json_encode(['newMsgs' => $newMsgs]);
     }
     return false;//return false if no new messages
-}), 'new-msgs');
+}));
+// echo 'data: '. '--------'. mt_rand(1, 1000). "\n\n";
+flush();
