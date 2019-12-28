f82acee8bbf701f1f3dad8430aee61af45c661e4
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2019-12-28 14:00:27 +0530

    Use usleep now

--- w.php
+++ w.php
@@ -1,9 +1,12 @@
 <?php
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
-
-$time = date('r');
-echo "data: The server time is: {$time}\n\n";
-flush();
-
-sleep(5);
+$i = 1;
+while ($i <= 10) {
+    $time = date('r');
+    echo "data: The server time is: {$time}\n\n";
+    ob_flush();
+    flush();
+    usleep(2000000);
+    $i++;
+}
