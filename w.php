6519928defd47603bb7015dc2966b7a81968f817
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2019-12-30 12:38:53 +0530

    Add connection keep alive header + print, rather than echo, and add ID

--- w.php
+++ w.php
@@ -7,10 +7,13 @@ if (function_exists('apache_setenv')) {
 
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
+header('Connection: keep-alive');
+
 $i = 1;
 while ($i <= 10) {
+    print "id: $id\n";
     $time = date('r');
-    echo "data: The server time is: {$time}\n\n";
+    echo "data: The server time is: {$time}\n";
     ob_flush();
     flush();
     usleep(2000000);
