cd0d268b08fbba5df9e31ef49fe569e272e53163
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2020-01-22 13:10:07 +0530

    Dry run

---
+++ slack/incoming/curl.php
@@ -0,0 +1,35 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://hooks.slack.com/services/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",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "{\"text\":\"Again, hello!!\"}",
+  CURLOPT_HTTPHEADER => array(
+    "Accept: */*",
+    "Accept-Encoding: gzip, deflate",
+    "Cache-Control: no-cache",
+    "Connection: keep-alive",
+    "Content-Type: application/json",
+    "Host: hooks.slack.com",
+    "cache-control: no-cache"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+
